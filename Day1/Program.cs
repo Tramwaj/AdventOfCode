e94aeadee738de48ffd4f04404047d3341a8749f
Michał Salamon <ms055452@student.ath.edu.pl>
2019-12-03 23:47:39 +0100

    not finished

---
+++ Day1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AoC19_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadAllLines(@"G:\input.txt");
+            int sum=0;
+            foreach (var item in input)
+            {
+                int module = Convert.ToInt32(item);
+                sum += module / 3 - 2;
+            }
+            int fuelWeight=sum;            
+            do
+            {
+                fuelWeight = fuelWeight / 3 - 2;
+                if (fuelWeight>0)
+                    sum += fuelWeight;                
+            } while (fuelWeight >= 0);
+            Console.WriteLine(sum);
+        }
+    }
+}
+//3429947
+//5144878
