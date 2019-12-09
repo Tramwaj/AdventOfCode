304d6ed3091e4b1ac09a267eb11dbec605ca76a8
Michał Salamon <ms055452@student.ath.edu.pl>
2019-12-10 00:21:17 +0100

    make sample work, but not real data

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -8,55 +8,94 @@ namespace Day6
     class Node
     {
         Node parent;
-        Node[] children;
-        int satellites;
+        List<Node> children;
+        public int satellites;
         //public void Add
         public Node()
         {
-
+            children = new List<Node>();
         }
         public Node(Node _parent)
         {
-            parent = _parent;            
+            parent = _parent;
+            children = new List<Node>();
+            //satellites = 1;
+        }
+        public void AddChild(Node child)
+        {
+            children.Add(child);
+            Update();
+        }
+        //private void Update() { }
+        internal void Update()
+        {
+            satellites = children.Select(x => (x.satellites + 1)).Sum();
+            if (parent != null) parent.Update();
         }
     }
     class Program
     {
-        static void Main(string[] args)
-        {
-            //Choose one:
-            //var Data =getData(); //real data
-            var Data = new List<string> { "COM)B","B)C","C)D","D)E","E)F","B)G","G)H","D)I","E)J","J)K","K)L",}; //sample data
-            var tree = new Dictionary<string, Node>();
-            foreach (var relationship in Data)
-            {
-                var relation = relationship.Split(')');
-                Console.WriteLine(relation[0] + " / " + relation[1]);
-                string parentName = relation[0];
-                string childName = relation[1];
-                AddNodes(parentName, childName,tree);
-
-            }
-        }
-        static void AddNodes(string parent, string child, Dictionary<string,Node> tree)
+        static int count=0;
+        static void AddNodes(string parent, string child, Dictionary<string, Node> tree)
         {
             if (!tree.ContainsKey(parent) && !tree.ContainsKey(child))
             {
                 tree.Add(parent, new Node());
                 tree.Add(child, new Node(tree[parent]));
+                
                 //tree[parent].
             }
+            if (tree.ContainsKey(parent)&&!tree.ContainsKey(child))
+            {
+                tree.Add(child, new Node(tree[parent]));               
+            }
+            if (!tree.ContainsKey(parent)&&tree.ContainsKey(child))
+            {
+                tree.Add(parent, new Node());                
+            }
+            tree[parent].AddChild(tree[child]);
+            //++count;
+            //Console.WriteLine("{0}: Parent {1} added child {2}",count,parent,child);
         }
+        static void Main(string[] args)
+        {
+            //Choose one:
+            //var Data =getData(); //real data
+            var Data = new List<string> { "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "D)J", "J)K", "K)L", }; //sample data
+            var tree = new Dictionary<string, Node>();
+            foreach (var relationship in Data)
+            {
+                if (relationship != null)
+                {
+                    var relation = relationship.Split(')');
+                    //Console.WriteLine(relation[0] + " / " + relation[1]);
+                    string parentName = relation[0];
+                    string childName = relation[1];
+                    AddNodes(parentName, childName, tree);
+                }
+            }
+            int sum=0;
+            foreach (var body in tree)
+            {
+                sum += body.Value.satellites;
+                //Console.WriteLine(body.Value.satellites.ToString());
+            }
+            Console.WriteLine(sum);
+        }
+        
 
         static List<string> getData()
         {
             var data = new List<string> { };
-            var path = @"C:\Users\Admin\source\repos\AdventOfCode\Day6\input.txt";
-            var file = new StreamReader(path);
+            //var path = @"C:\Users\Admin\source\repos\AdventOfCode\Day6\input.txt";
+            string path = @"C:\Users\User\source\repos\AoC19\Day6\input.txt";
+           var file = new StreamReader(path);
             string line = file.ReadLine();
+            data.Add(line);
             while (line != null)
             {
                 line = file.ReadLine();
+                data.Add(line);
             }
             return data;
         }
