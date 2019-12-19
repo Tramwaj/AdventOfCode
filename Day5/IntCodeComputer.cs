c5d8a3e2b87db02a8f7587e832b082103fcd8cb5
Michał Salamon <ms055452@student.ath.edu.pl>
2019-12-19 15:20:22 +0100

    finish day 1 , day 2 in progress

--- Day5/IntCodeComputer.cs
+++ Day5/IntCodeComputer.cs
@@ -6,46 +6,83 @@ namespace Day5
 {
     class IntCode
     {
-        public static int Compute(int userInput, int[] program)
+        int opcode, param1, param2;
+        public int input;
+        List<int> output;
+        int[] program;
+        int counter = 0;
+        public IntCode(int _userInput, int[] _program)
         {
-            int input = userInput;
-            var output = new List<int>();
-            int counter = 0;
-            int opcode, param1, param2, param3;
+            input = _userInput;
+            program = _program;
+            output = new List<int>();
+        }
+        public int Compute()
+        {
             int mode1, mode2, mode3;
             do
             {
-                mode3 = program[counter] / 10000;
-                mode2 = program[counter] / 1000;
-                mode1 = program[counter] / 100;
+                int ABCDE = program[counter];
+                mode3 = ABCDE / 10000;
+                ABCDE -= mode3 * 10000;
+                mode2 = ABCDE / 1000;
+                ABCDE -= mode2 * 1000;
+                mode1 = ABCDE / 100;
                 opcode = program[counter] % 100;
-                if (opcode == 99) break;    
-                if (mode2 == 0) param2 = program[program[counter + 2]];
-                else param2 = program[counter + 2];
-                if (mode1 == 0) param1 = program[program[counter + 1]];
-                else param1 = program[counter + 1];
-
-                if (mode3 == 0) 
-                    program[program[counter + 3]] = Resolve();
-                else 
-                    program[counter + 3] = Resolve();                                
-                if (opcode == 1)
-                {
-
-                    program[program[counter + 3]] = program[program[counter + 1]] + program[program[counter + 2]];
-                    counter += 4;
-                }
-                else if (opcode == 2)
-                {
-                    program[program[counter + 3]] = program[program[counter + 1]] * program[program[counter + 2]];
-                    counter += 4;
-                }
+                if (opcode == 99) break;
+                if (opcode <= 2 || opcode > 6) TernaryOperation(mode1, mode2, mode3);
+                if (opcode > 2) UnaryOperation(mode1);
+                if (opcode == 5 || opcode == 6) BinaryOperation(mode1, mode2);
             } while (opcode != 99);
             return program[0];
         }
-        private static int Resolve()
+        private void BinaryOperation(int mode1,int mode2)
         {
-            if 
+            if (mode1 == 0) param1 = program[program[counter + 1]];
+            else param1 = program[counter+1];
+            param2 = program[counter + 2];
+            bool paramIsZero= (param1 == 0);
+            if (paramIsZero && opcode == 5) counter = param2;
+            if (!paramIsZero && opcode == 6) counter = param2;
+        }
+        private void TernaryOperation(int mode1, int mode2, int mode3)
+        {
+            int _result = 1337;            
+            if (mode2 == 0) param2 = program[program[counter + 2]];
+            else param2 = program[counter + 2];
+            if (mode1 == 0) param1 = program[program[counter + 1]];
+            else param1 = program[counter + 1];
+
+            if (opcode == 1)
+            {
+                _result = param1 + param2;
+            }
+            if (opcode == 2)
+            {
+                _result = param1 * param2;
+            }
+            if (opcode == 7) _result = Convert.ToInt32(param1 < param2);
+            if (opcode == 8) _result = Convert.ToInt32(param1 > param2);
+            if (mode3 == 0) program[program[counter + 3]] = _result;
+            if (mode3 == 1) program[counter + 3] = _result;
+            counter += 4;
+        }
+        private void UnaryOperation(int mode)
+        {
+            if (opcode == 3)
+            {
+                if (mode == 0) program[program[counter + 1]] = input;
+                if (mode == 1) Console.WriteLine("Cannot write to value");
+            }
+            if (opcode == 4)
+            {
+                int _out = 1337;
+                if (mode == 0) _out = program[program[counter + 1]];
+                if (mode == 1) _out = program[counter + 1];
+                output.Add(_out);
+                Console.WriteLine("Output no. {0}: {1}", output.Count, _out);
+            }
+            counter += 2;
         }
     }
 }
--- Day5/Program.cs
+++ Day5/Program.cs
@@ -12,19 +12,24 @@ namespace Day5
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)+"//input.txt";
             var file = new StreamReader(path);
             string line = file.ReadLine();
-            Console.WriteLine(line);
+            //Console.WriteLine(line);
             int[] data = line.Split(",").Select(x=>Convert.ToInt32(x)).ToArray();
-            foreach (var inp in data)
-            {
-                Console.WriteLine("{0}  ",inp);
-            }
+            //foreach (var inp in data)
+            //{
+            //    Console.WriteLine("{0}  ",inp);
+            //}
             return data;
         }
         //static bool IntCode(int userInput, int[] program)        {                 }
         static void Main(string[] args)
         {
+
+             int[] input = getData();
+            //int[] input = { 1002, 4, 3, 4, 33 };
+            var computer = new IntCode(5, input);
+            computer.Compute();
+            //Console.WriteLine(input[0]);
             
-            int[] input = getData();
             
         }
     }
